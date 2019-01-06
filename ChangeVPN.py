a16c8d52aa06126f4a6021037af70b79786844ee
koddebugging <codedebugging@outlook.ie>
2019-01-06 17:58:27 +0100

    Adding Python script to automatically change the VPN connection at random.

---
+++ ChangeVPN.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+"""Randomly connections to a random VPN profile (if any are found).
+
+Uses NetworkManager (https://developer.gnome.org/NetworkManager/stable/gdbus-org.freedesktop.NetworkManager.html#) to
+enumerate the devices and connections. First, we enumerate the wireless devices to ensure that we have one. Next, we
+enumerate the VPN connections and put them into a list. After that, we randomly select one of the VPN connections to
+connect to. Once we've accomplished this, we disconnect the current VPN connection (if one is found) and connect to
+the randomly chosen VPN connection.
+
+REQUIREMENTS:
+    python-networkmanager
+
+TO RUN:
+    python NetworkManager_VPN.py
+"""
+
+__author__ = "felsokning"
+__copyright__ = "Copyright 2019"
+__license__ = "MIT"
+
+import random
+import time
+# Externals
+import NetworkManager
+
+# Find all of the VPN connections on the machine.
+vpns = list()
+connections = NetworkManager.Settings.ListConnections()
+for c in connections:
+    if "vpn" in c.GetSettings()['connection']['type']:
+        path = c.object_path
+        vpns.append(path)
+
+# If there are no VPN connections, there's no point in proceeding.
+if vpns.__len__() > 0:
+    # We find the Wireless Network Interface.
+    # If you're running some kind of weird, three wireless network cards situation, then...
+    # Change this code to work in your use-case scenario.
+    devices = NetworkManager.Device.all()
+    for d in devices:
+        if "wifi" in d.Driver:
+            wireless_device = d
+
+    # Validate that we found a wireless network interface
+    if wireless_device is not None:
+        # Get the currently active VPN connection.
+        # If you're running some kind of weird, three active VPNs scenario, then...
+        # Change this code to work in your use-case.
+        active = NetworkManager.NetworkManager.ActiveConnections
+        for a in active:
+            if "vpn" in a.Type:
+                current_vpn = a
+
+        # Choose a random one to connect to.
+        random_int = random.randint(0, (vpns.__len__() - 1))
+        random_vpn = vpns.__getitem__(random_int)
+        new_connection = NetworkManager.Connection(random_vpn)
+
+        # Validate that we have a current VPN connection to disconnect from before we do.
+        if current_vpn is not None:
+
+            # Disconnect the old & busted.
+            NetworkManager.NetworkManager.DeactivateConnection(current_vpn)
+
+            # To prevent collision in NetworkManager, we allow background clean-up before reconnecting.
+            time.sleep(10)
+
+        # Connect the new hotness.
+        print("Connecting to: {}".format(random_vpn))
+        NetworkManager.NetworkManager.ActivateConnection(new_connection, wireless_device, "/")
+
+    # No wireless interfaces were found, so let's abort.
+    else:
+        raise Exception("No wireless interfaces were found.")
+
+# No VPN connections were found, so let's abort.
+else:
+    raise Exception("The hull has been breached and the science is leaking out! "
+                    "(We didn't find any valid VPN connections on this machine via NetworkManager.)")
--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Python
 Several scripts written in Python, to accomplish varying tasks.
 
-## MacChaningDaemon.py
-Creates a daemon to change the mac address on an interface once per hour (far too aggressive, I'm aware).
+## ChangeVPN.py
+Randomly connects to a VPN connection on the wireless interface via NetworkManager.
 
 ## CheckSockets.py
-Attempts to open a socket and, if blocked, then finds out what process in currently utilising that socket.
+Enumerates through ports (0-65534) and attempts to open a socket on each one. If blocked, the socket is added to a list and then we attempt to find out what process, precisely, is currently utilising that socket.
+
+## MacChaningDaemon.py
+Creates a daemon to change the mac address on an interface once per hour (far too aggressive, I'm aware).
 
 ## SvenskaVeckanNummer.py
 Original skeleton for the script that's used in Docker (see: Docker/SwedishWeekNumber)
